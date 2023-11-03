dd6a3bec517bd518d0fd338c66c58f2830800788
Waleed Alzahrani <aannsq23@gmail.com>
2023-11-04 01:02:32 +0300

    JS 4 Exercise 3

--- Script.js
+++ Script.js
@@ -14,75 +14,65 @@
                 : navItems.forEach(item => item.style.transitionDelay = '0s');
         }
 
-        const shoesCards = document.querySelectorAll('.shoes-card');
+        const shoeData = [
+            {
+                imageUrl: "./image/Screenshot-removebg-preview.png",
+                link: "./public/mans-brooks.html",
+                name: "Mans Brooks",
+            },
+            {
+                imageUrl: "./image/Screenshot-removebg-preview (2).png",
+                link: "./public/mens-asics.html",
+                name: "Mens Asics",
+            },
+            {
+                imageUrl: "./image/Screenshot-removebg-preview (1).png",
+                link: "./public/mens-hoka.html",
+                name: "Men Hoke",
+            },
+            {
+                imageUrl: "./image/CG-120-G-FULLBLK-2.jpg-removebg-preview.png",
+                link: "./public/mens-running-shoes.html",
+                name: "TYSON Pro",
+            },
+        ];
 
-        shoesCards.forEach(card => {
-            card.addEventListener('mouseenter', () => toggleCardLinks(card, true));
-            card.addEventListener('mouseleave', () => toggleCardLinks(card, false));
-        });
+        const generateProductCard = (product) => {
+            const card = document.createElement("span");
+            card.classList.add("shoes-card");
 
-        function toggleCardLinks(card, isActive) {
-            const cardLinks = card.querySelectorAll('a');
-            const cardImg = card.querySelector('img');
-            cardLinks.forEach(link => link.classList.toggle('active', isActive));
-            cardImg.classList.toggle('active', isActive);
-        }
+            const anchor = document.createElement("a");
+            anchor.href = product.link;
+
+            const img = document.createElement("img");
+            img.classList.add("shoes-img");
+            img.src = product.imageUrl;
+            img.alt = "shoes";
+
+            const watermark = document.createElement("img");
+            watermark.classList.add("watermark");
+            watermark.src = "./image/watermark .png";
+            watermark.alt = "Watermark for shoe shop";
+
+            const name = document.createElement("b");
+            name.textContent = product.name;
+
+            anchor.appendChild(img);
+            anchor.appendChild(watermark);
+            anchor.appendChild(document.createElement("br"));
+            anchor.appendChild(name);
+
+            card.appendChild(anchor);
+
+            return card;
+        };
+
+        const productCards = shoeData.map(generateProductCard);
+        const shoesContainer = document.querySelector(".shoes-container");
+        productCards.forEach((card) => shoesContainer.appendChild(card));
+
+        const filteredProducts = shoeData.filter((product) => product.name.includes("Mans"));
+
+        const totalProductLinks = shoeData.reduce((total, product) => total + product.link, "");
     });
 })();
-
-
-
-
-
-
-
-class Product {
-    constructor(name, imageUrl, link) {
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.link = link;
-    }
-}
-
-const products = [
-    new Product("Mans Brooks", "./image/Screenshot-removebg-preview.png", "./public/mans-brooks.html"),
-    new Product("Mens Asics", "./image/Screenshot-removebg-preview (2).png", "./public/mens-asics.html"),
-    new Product("Men Hoke", "./image/Screenshot-removebg-preview (1).png", "./public/mens-hoka.html"),
-    new Product("TYSON Pro", "./image/CG-120-G-FULLBLK-2.jpg-removebg-preview.png", "./public/mens-running-shoes.html"),
-];
-
-
-function generateProductCards(productList) {
-    const productsContainer = document.querySelector(".shoes-container");
-
-    productList.forEach((product) => {
-        const card = document.createElement("span");
-        card.classList.add("shoes-card");
-
-        const anchor = document.createElement("a");
-        anchor.href = product.link;
-
-        const img = document.createElement("img");
-        img.classList.add("shoes-img");
-        img.src = product.imageUrl;
-        img.alt = "shoes";
-
-        const watermark = document.createElement("img");
-        watermark.classList.add("watermark");
-        watermark.src = "./image/watermark .png";
-        watermark.alt = "Watermark for shoe shop";
-
-        const name = document.createElement("b");
-        name.textContent = product.name;
-
-        anchor.appendChild(img);
-        anchor.appendChild(watermark);
-        anchor.appendChild(document.createElement("br"));
-        anchor.appendChild(name);
-
-        card.appendChild(anchor);
-        productsContainer.appendChild(card);
-    });
-}
-
-generateProductCards(products);
